6089b52757f8f4d99991f489ab7a415d1fdf9d33
Weaver <weaverthree@gmail.com>
2026-03-06 09:53:28 -0800

    header_two_slots now calculates lengths of visual strings

--- world/utils.py
+++ world/utils.py
@@ -1,4 +1,5 @@
 import re
+from evennia.utils import display_len
 
 
 
@@ -36,11 +37,11 @@ def header_two_slot(width, slot1, slot2=None, headercolor="|R", color1="|w", col
     if slot2:
         header_left = f"{headercolor}--< {color1}{slot1} {headercolor}>-"
         header_right = f"{headercolor}-< {color2}{slot2} {headercolor}>--|n"
-        fill = width - len(header_left) - len(header_right) + 4*len(headercolor) + len(color1) + len(color2) + 2
+        fill = width - display_len(header_left) - display_len(header_right)
     else:
         header_left = f"{headercolor}--< {color1}{slot1} {headercolor}>-"
         header_right = "|n"
-        fill = width - len(header_left) + 2*len(headercolor) + len(color1)
+        fill = width - display_len(header_left) + display_len(headercolor)
 
 
     return "".join((header_left, "-" * fill, header_right))
